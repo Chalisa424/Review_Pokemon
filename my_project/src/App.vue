257eb5570a3261b45a93247c73c2dc8a82b8fc8d
Chalisa424 <>
2025-08-18 10:02:27 +0700

    install axios

--- my_project/src/App.vue
+++ my_project/src/App.vue
@@ -5,17 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   
   <HelloWorld msg="Vite + Vue" />
+  </div>
 </template>
 
 <style scoped>
